8dfc7cea62c62faf9cc011071d67983256bf0c01
skwan <94733484+skwantam@users.noreply.github.com>
2021-11-20 19:02:55 +0800

    #1 test for 1 (#1)
    * Palindrome number
    
    * gitignore
    
    * README

---
+++ javaOj/src/test/java/com/tsj/oj/PalindromeNumTest.java
@@ -0,0 +1,32 @@
+package com.tsj.oj;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class PalindromeNumTest extends TestCase{
+    public PalindromeNumTest(){}
+    public PalindromeNumTest(String name){
+        super(name);
+    }
+
+    @Override
+	public void setUp(){
+	}
+
+    @Override
+	public void tearDown(){}
+
+    public void testIsPalindrome(){
+        assertEquals(true, PalindromeNum.isPalindrome(100001));
+    }
+
+	public static Test suite(){
+        TestSuite testSuite=new TestSuite("All Test From TestCaseExample");
+        // 第一种方法
+        // testSuite.addTestSuite(PalindromeNumTest.class);
+        Test test = TestSuite.createTest(PalindromeNumTest.class, "testIsPalindrome");
+        testSuite.addTest(test);
+        return testSuite;
+    }
+}
