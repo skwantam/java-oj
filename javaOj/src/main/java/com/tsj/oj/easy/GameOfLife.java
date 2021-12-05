3bf8caa447cfc24bd1c8726a44f428892205d502
skwam <13120770959@163.com>
2021-12-05 20:58:17 +0800

    deputy

--- javaOj/src/main/java/com/tsj/oj/easy/GameOfLife.java
+++ javaOj/src/main/java/com/tsj/oj/easy/GameOfLife.java
@@ -7,7 +7,11 @@ public class GameOfLife {
   public static void main(String[] args) {
     GameOfLife gol = new GameOfLife();
     int[][] board = new int[][] { { 0, 0, 0 }, { 1, 0, 1 }, { 0, 1, 1 }, { 0, 1, 0 }, };
-    System.out.println(gol.getNeighbor(0, 0, board));
+    gol.printBoard(board);
+    for(int i=0; i<10; i++){
+      gol.oneStep(board);
+      gol.printBoard(board);
+    }
   }
 
   public void oneStep(int[][] board) {
@@ -30,7 +34,7 @@ public class GameOfLife {
       for (int j = 0; j < m; j++) {
         if (board[i][j] == -1) {
           board[i][j] = 1;
-        } else if (board[i][j] == 2) {
+        } else if (board[i][j] == -2) {
           board[i][j] = 0;
         }
       }
@@ -41,12 +45,10 @@ public class GameOfLife {
     int n = board.length, m = board[0].length;
     if (i < 0 || i >= n || j < 0 || j >= m)
       return 0;
-
     int top = Math.max(0, i - 1);
     int bottom = Math.min(n - 1, i + 1);
     int left = Math.max(0, j - 1);
     int right = Math.min(m - 1, j + 1);
-
     int count = 0;
     for (int x = top; x <= bottom; x++) {
       for (int y = left; y <= right; y++) {
@@ -54,11 +56,20 @@ public class GameOfLife {
           count++;
       }
     }
-
     if (board[i][j] == 1 || board[i][j] == -1) {
       count--;
     }
-
     return count;
+  }
+
+  public void printBoard(int[][] board){
+    System.out.println("==============================>");
+    for(int i=0; i<board.length; i++){
+      for(int j=0; j<board[0].length; j++){
+        System.out.print(board[i][j] + " " );
+      }
+      System.out.println();
+    }
+    System.out.println("<==============================");
   }
 }
